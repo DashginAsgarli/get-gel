1c87d6a179d7a87c16af33a70a50e4bec3391a82
DashginAsgarli <dashqinasgarli@gmail.com>
2026-05-03 02:04:14 +0400

    refactor: remove unused files and clean up project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import React from 'react';
-import FonLine from "./assets/fonline.svg";
-import Navbar from './component/header/navbar';
-import Aside from './component/aside/aside';
-import HomePage from './component/main/homePage';
-
+import React from 'react'
 
 function App() {
   return (
-    <section className="relative bg-[#1F242E]">
-      <img src={FonLine} alt="background-line" className="absolute w-full top-0 left-0 z-0 " />
+    <>
 
-      <div className='relative z-10 px-[120px] py-[130px]'>
-        <div className='bg-[#111111] w-full rounded-[40px] p-[45px]'>
-          <Navbar />
-
-          <div className='flex items-center gap-8'>
-            <div>
-              <Aside />
-            </div>
-
-            <div>
-              <HomePage />
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </section>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
